6c068fe4662175bfd3f2038ebd0b5e9d30dccbc2
Artquito <42540286+Artquito@users.noreply.github.com>
2025-05-09 14:00:50 +0800

    add hover animation to navigation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,16 +5,16 @@ const Header = () => {
     <header className="z-1 flex items-center justify-between px-30 py-8 bg-white">
       <Blob1 cssClass="absolute w-140 -top-85 -left-17 -rotate-170 z-2"></Blob1>
       <div className="z-3">
-        <h3 className="text-white text-2xl font-bold">HOME</h3>
+        <h3 className="text-white text-3xl font-bold">BtoGo</h3>
       </div>
       <nav>
         <ul className="flex gap-6">
-          <li>
+          <li className="hover:scale-110 transition-transform">
             <a href="/" className="font-bold text-gray-800 hover:text-blue-600">
               ABOUT
             </a>
           </li>
-          <li>
+          <li className="hover:scale-110 transition-transform">
             <a
               href="/about"
               className="font-bold text-gray-800 hover:text-blue-600"
@@ -22,7 +22,7 @@ const Header = () => {
               PRICING
             </a>
           </li>
-          <li>
+          <li className="hover:scale-110 transition-transform">
             <a
               href="/services"
               className="font-bold text-gray-800 hover:text-blue-600"
@@ -30,10 +30,10 @@ const Header = () => {
               CONTACT
             </a>
           </li>
-          <li>
+          <li className="hover:scale-110 transition-transform">
             <a
               href="/contact"
-              className="gradient-border text-white font-bold  hover:bg-blue-100 rounded-sm py-3 px-14"
+              className="gradient-border text-white font-bold hover:bg-blue-100 rounded-sm py-3 px-14 "
             >
               LOGIN
             </a>
