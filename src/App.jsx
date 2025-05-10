42d3fb17e97d11f03f3f6313e93c6d9c351dae4f
Artquito <42540286+Artquito@users.noreply.github.com>
2025-05-10 08:24:17 +0800

    Fix scroll bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-// import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
 import Header from "./components/header.jsx";
 import Hero from "./components/hero.jsx";
+import About from "./components/about.jsx";
 
 const App = () => {
   return (
-    <div className="relative overflow-x-hidden">
+    <div className="relative overflow-hidden">
       <Header />
       <Hero />
+      <About />
     </div>
   );
 };
